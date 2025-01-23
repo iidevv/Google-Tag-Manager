8a25af881460fd540111afbeee4b65a50a849b6d
spanandequipment <ilya.ilyich1105@gmail.com>
2025-01-23 15:38:25 -0800

    isCompletedOrder

--- src/Model/Order.php
+++ src/Model/Order.php
@@ -45,14 +45,7 @@ abstract class Order extends \XLite\Model\Order
     {
         parent::setShippingStatus($shippingStatus);
 
-        if (!$this->getShippingStatus() || !$this->getPaymentStatus())
-            return;
-
-        $shippingStatus = $this->getShippingStatus()->getCode();
-
-        $paymentStatus = $this->getPaymentStatus()->getCode();
-
-        if ($paymentStatus === \XLite\Model\Order\Status\Payment::STATUS_PAID && $shippingStatus === \XLite\Model\Order\Status\Shipping::STATUS_SHIPPED) {
+        if ($this->isCompletedOrder()) {
             $tracking = new BackendTracking;
 
             $order = \XLite\Core\Database::getRepo('XLite\Model\Order')->find($this->getOrderId());
@@ -63,6 +56,31 @@ abstract class Order extends \XLite\Model\Order
 
             $tracking->doPurchase($order);
         }
+    }
+
+    protected function isCompletedOrder(): bool
+    {
+        $paymentStatus = $this->getPaymentStatus()?->getCode();
+        $shippingStatus = $this->getShippingStatus()?->getCode();
+        $oldShippingStatus = $this->oldShippingStatus?->getCode();
+
+        if (!$oldShippingStatus || $oldShippingStatus == $shippingStatus) {
+            return false;
+        }
+
+        if (!$shippingStatus || !$paymentStatus) {
+            return false;
+        }
+
+        if($paymentStatus === \XLite\Model\Order\Status\Payment::STATUS_PAID && $shippingStatus === \XLite\Model\Order\Status\Shipping::STATUS_SHIPPED) {
+            return true;
+        }
+
+        if($paymentStatus === \XLite\Model\Order\Status\Payment::STATUS_PAID && $shippingStatus === \XLite\Model\Order\Status\Shipping::STATUS_DELIVERED) {
+            return true;
+        }
+
+        return false;
     }
 
 }
